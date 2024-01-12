e359ae2783ef82e81b552db14d7d42366e8501cf
Shelley McHardy <shelleymcq.dev@gmail.com>
2024-01-12 15:32:36 -0500

    intentional failing test

--- cypress/e2e/my-tests/third-test.cy.js
+++ cypress/e2e/my-tests/third-test.cy.js
@@ -1,6 +1,6 @@
 describe("what happens when a test fails", () => {
-  it("h3 contains test fails", () => {
+  it("h3 does not contain test fails", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("h3").contains("test fails");
+    cy.get("h3").contains("test failed");
   });
 });
